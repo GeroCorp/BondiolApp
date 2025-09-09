4e76269aee436f00a113b912ca36a99cb35eff57
TobiasEscobar <tobyescobar19@gmail.com>
2025-09-09 17:07:17 -0300

    inicio proyecto

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/services/supabase';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage {
+  email: string | null = null;
+
+  constructor(private router: Router, private authService: AuthService, private toastController: ToastController) {
+    this.email = history.state['email'] ?? null;
+  }
+
+  async logout() {
+    await this.authService.logout();
+
+    this.router.navigate(['/login']); // redirigir al login
+    this.showToast('Sesión cerrada correctamente');
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2500,
+      color: 'medium',
+      position: 'bottom',
+    });
+    await toast.present();
+  }
+}
---
+++ src/app/services/supabase.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient, AuthError } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(
+      SUPABASE_URL, SUPABASE_ANON_KEY
+    );
+  }
+
+  async login(email: string, password: string) {
+    try {
+      const { data, error } = await this.supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) {
+        // Lanzamos un error con mensaje más claro
+        throw new Error(this.mapAuthError(error));
+      }
+
+      return data;
+
+    } catch (err: any) {
+      console.error('Error en login:', err);
+      throw new Error(this.mapAuthError(err));
+    }
+  }
+
+  async logout() {
+    try {
+      const { error } = await this.supabase.auth.signOut();
+      if (error) throw new Error(this.mapAuthError(error));
+    } catch (err) {
+      console.error('Error en logout:', err);
+      throw new Error('No se pudo cerrar sesión.');
+    }
+  }
+
+  getUser() {
+    return this.supabase.auth.getUser();
+  }
+
+  // Traducir errores de Supabase a mensajes más claros
+  private mapAuthError(error: AuthError): string {
+    switch (error.message) {
+      case 'Invalid login credentials':
+        return 'El correo y/o la contraseña son incorrectos.';
+      case 'Email not confirmed':
+        return 'Debes confirmar tu correo antes de iniciar sesión.';
+      case 'missing email or phone':
+        return 'Complete todos los campos antes de ingresar'
+      default:
+        return 'Error de autenticación: ' + error.message;
+    }
+  }
+}
