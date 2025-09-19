cfa88d8a80856046311f691370c9ab80bf1461e8
valentino pages <valentinopages664@gmail.com>
2025-09-19 10:10:58 -0300

    administrar clientes duenio y supervisor

---
+++ src/app/tabs-cliente/tabs-cliente.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-cliente',
+  templateUrl: './tabs-cliente.component.html',
+  styleUrls: ['./tabs-cliente.component.scss'],
+})
+export class TabsClienteComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
